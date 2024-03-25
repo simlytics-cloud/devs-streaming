6e8e20a28837afc43dcaa3ccee0848afa64998bc
robert.kewley <rob@simlytics.clouod>
2024-03-25 07:23:26 -0400

    Added documentation to PDEVSModel.java

--- src/main/java/devs/PDEVSModel.java
+++ src/main/java/devs/PDEVSModel.java
@@ -21,6 +21,11 @@ import devs.msg.Bag;
 import devs.msg.PortValue;
 import devs.msg.time.SimTime;
 
+/**
+ * This class implements a DEVS atomic model
+ * @param <T> the time type used by the model
+ * @param <S> the internal state type for the modelState
+ */
 public abstract class PDEVSModel<T extends SimTime, S> {
 
   protected S modelState;
@@ -33,9 +38,22 @@ public abstract class PDEVSModel<T extends SimTime, S> {
     this.modelIdentifier = modelIdentifier;
   }
 
-
+  /**
+   * The DEVS internal state transition function.  This function is called when the model is executing a
+   * schedule internal transition that occurs with the passage of time
+   * @param currentTime the current time of the transition
+   */
   protected abstract void internalStateTransitionFunction(T currentTime);
 
+  /**
+   * The DEVS external state transition function.  This function is called when an external event arrives at
+   * one of the model's input ports.  Note two important considerations.  First, the type of event is not
+   * known in advance, so this function will have to use the {@link PortValue PortValues} contained in the
+   * input {@link devs.msg.Bag Bag} to identify the type of event and process it appropriately.  In addition,
+   * more than one input is possible at an intant in time.
+   * @param currentTime the current time of the transition
+   * @param bag the bag of inputs that has arrived at the current time.
+   */
   protected abstract void externalSateTransitionFunction(T currentTime, Bag bag);
 
   protected abstract void confluentStateTransitionFunction(T currentTime, Bag bag);
