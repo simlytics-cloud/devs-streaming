a468aa66bb5509d5c984df1af26086357a535e85
Rob Kewley <rob@simlytics.cloud>
2024-07-26 23:59:15 +0000

    Updated ObjectMapper for Java time serialization.

--- src/main/java/devs/utils/DevsObjectMapper.java
+++ src/main/java/devs/utils/DevsObjectMapper.java
@@ -22,6 +22,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import devs.msg.PortValue;
 import devs.msg.PortValueDerserializer;
 
@@ -36,6 +38,7 @@ public class DevsObjectMapper {
     return new ObjectMapper()
         .registerModule(new Jdk8Module())
         .registerModule(new GuavaModule())
+        .registerModule(new JavaTimeModule())
         .registerModule(portValueModule);
   }
 }
